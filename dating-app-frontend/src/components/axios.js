16cdbf2b86d39c087f7d458d9ec441ebc143e286
Vincnet J Lettau II <vjerome2@live.com>
2021-12-27 21:53:14 -0500

    after backend to heroku

--- dating-app-frontend/src/components/axios.js
+++ dating-app-frontend/src/components/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8001",
+  baseURL: "https://dating-mern-backend.herokuapp.com",
 });
 export default instance;
